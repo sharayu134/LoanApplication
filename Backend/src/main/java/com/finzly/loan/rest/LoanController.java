f01272bf7b3154a481b69a0f44a6611d6381b768
sharayu134 <ysharayu18@gmail.com>
2020-12-16 14:54:33 +0530

    Added Server side Pagination

--- Backend/src/main/java/com/finzly/loan/rest/LoanController.java
+++ Backend/src/main/java/com/finzly/loan/rest/LoanController.java
@@ -3,8 +3,11 @@ package com.finzly.loan.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +36,32 @@ public class LoanController {
 	public void add(@RequestBody Loan  loan) {	
 		loanService.save(loan);
 }
+	@GetMapping("/loans/page/{pageNumber}/page_size/{pageSize}")
+	public String findPaginatedList(@PathVariable (value="pageNumber") int pageNumber,@PathVariable (value="pageSize") int pageSize, Model model){
+		
+		Page<Loan> page=loanService.findPaginatedLoans(pageNumber, pageSize); 
+		List<Loan> loanList=page.getContent();
+
+		model.addAttribute("currentPage",pageNumber);
+		model.addAttribute("totalPages",page.getTotalPages());
+		model.addAttribute("titalItems", page.getTotalElements());
+		model.addAttribute("loanList",loanList);
+		
+		return "index"; 
+	}
+	
+	@GetMapping("/loans/page/{pageNumber}/records_per_page/{pageSize}")
+	public List<Loan> findPaginatedLoanList(@PathVariable (value="pageNumber") int pageNumber,@PathVariable (value="pageSize") int pageSize, Model model){
+		
+		Page<Loan> page=loanService.findPaginatedLoans(pageNumber, pageSize); 
+		List<Loan> loanList=page.getContent();
+
+		model.addAttribute("currentPage",pageNumber);
+		model.addAttribute("totalPages",page.getTotalPages());
+		model.addAttribute("titalItems", page.getTotalElements());
+		model.addAttribute("loanList",loanList);
+		
+		return loanList; 
+	}
+	
 }
--- Backend/src/main/java/com/finzly/loan/service/LoanService.java
+++ Backend/src/main/java/com/finzly/loan/service/LoanService.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.finzly.loan.dao.LoanRepository;
@@ -44,5 +47,10 @@ public class LoanService {
 		loanRepository.save(loan);
 		PaymentScheduleService.save(loan);	
 		return loan;
+	}
+	
+	public Page<Loan> findPaginatedLoans(int pageNumber, int pageSize){
+		Pageable pageable = PageRequest.of(pageNumber-1,pageSize);
+		return this.loanRepository.findAll(pageable);
 	}
 }
